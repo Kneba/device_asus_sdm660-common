d36389df0d90e07633ea78ae9dee72a90d45fdc0
Danny Trunk <dtrunk90@gmail.com>
2022-02-28 16:46:49 +0530

    sdm660-common: Add shim for missing symbol in lib-imsvt.so
    Signed-off-by: Kneba <abenkenary3@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -435,6 +435,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
+    lib-imsvtshim \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
@@ -487,3 +488,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
